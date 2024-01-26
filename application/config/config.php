75c7747b39a3735c49f748ed89c6917b640027e6
Anees Afzal <aneesmug2007@yahoo.com>
2024-01-26 22:21:16 +0300

    change url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/github/inv_pro/';
+$config['base_url'] = 'http://localhost/GitHub/inv_pro/';
 
 /*
 |--------------------------------------------------------------------------
